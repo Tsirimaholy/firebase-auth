a174aaaf2791ae163e306fee1e8ef0f0e85b2c45
Tsirimaholy <hei.tsirimaholy@gmail.com>
2022-08-19 00:56:41 +0300

    feat: add NotFound page to route

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ function App() {
                     }
                 />
                 <Route path="login" element={<Login/>}/>
+                <Route path="*" element={<NotFound/>}/>
             </Routes>
         </div>
     );
