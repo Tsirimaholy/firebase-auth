8b7b2ab6ba56df00cc81782753f16a2a20f6a658
Tsirimaholy <hei.tsirimaholy@gmail.com>
2022-08-19 17:36:06 +0300

    add env info

---
+++ src/secret.ts
@@ -0,0 +1,8 @@
+export const secret = {
+    apiKey: "AIzaSyCkvc8l0mp3EDLas1T234mbi7b6JmUbXSs",
+    authDomain: "auth-firebase-533a6.firebaseapp.com",
+    projectId: "auth-firebase-533a6",
+    storageBucket: "auth-firebase-533a6.appspot.com",
+    messagingSenderId: "361323617669",
+    appId: "1:361323617669:web:c2289aeb7591b46eb878ec"
+}
